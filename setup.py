b11b1ae529d2b542cbe5064470cf8573d7015f6e
RobertJN <robertjnies@gmail.com>
2021-10-17 23:09:43 -0400

    Still trying to fix MANIFEST.in

--- setup.py
+++ setup.py
@@ -6,12 +6,13 @@ setup(
     author='Robert Nies',
     author_email='robertjnies@gamil.com',
     # Needed to actually package something
+
     include_package_data=True,
     install_requires=['pandas'],
     extras_require={'dash': ['dash', 'plotly', 'dash_bootstrap_components>=1.0.0'],
                     'dash-notebook': ['dash', 'plotly', 'dash_bootstrap_components>=1.0.0', 'jupyter-dash']},
     # *strongly* suggested for sharing
-    version='0.0.5',
+    version='0.0.6',
     # The license can be anything you like
     license='MIT',
     description='A set of functions and demos to make machine learning projects easier to understand.',
