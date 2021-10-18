7747af32e37976c2341eec750e09c6e615da1a76
RobertJN <robertjnies@gmail.com>
2021-10-17 23:06:37 -0400

    Installers are hard

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
 from setuptools import setup
-import os
-
-
-include = []
-for item in list(os.walk('MLVisualizationTools')):
-    if 'pycache' not in item[0]:
-        include.append(item[0].replace('\\', '.'))
-
 
 setup(
     name='MLVisualizationTools',
@@ -14,12 +6,12 @@ setup(
     author='Robert Nies',
     author_email='robertjnies@gamil.com',
     # Needed to actually package something
-    packages=include,
+    include_package_data=True,
     install_requires=['pandas'],
     extras_require={'dash': ['dash', 'plotly', 'dash_bootstrap_components>=1.0.0'],
                     'dash-notebook': ['dash', 'plotly', 'dash_bootstrap_components>=1.0.0', 'jupyter-dash']},
     # *strongly* suggested for sharing
-    version='0.0.3',
+    version='0.0.4',
     # The license can be anything you like
     license='MIT',
     description='A set of functions and demos to make machine learning projects easier to understand.',
