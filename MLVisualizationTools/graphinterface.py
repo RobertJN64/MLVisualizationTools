9fcdf0ba71ab12f73a48fc7e6f4453297da54103
RobertJN <robertjnies@gmail.com>
2022-03-30 19:19:18 -0400

    3.10, replaced append with concat

--- MLVisualizationTools/graphinterface.py
+++ MLVisualizationTools/graphinterface.py
@@ -13,6 +13,7 @@ from MLVisualizationTools import Colorizers
 from MLVisualizationTools.backend import GraphData
 from typing import Optional
 import copy
+import pandas as pd
 
 
 class WrongDataFormatException(Exception):
@@ -163,7 +164,7 @@ def plotlyGrid(data: GraphData, title="", legend: bool=True, sizekey: Optional[s
                 row = copy.deepcopy(orig_row)
                 row[data.colorkey] = color.color
                 row[data.sizekey] = 0
-                datavals.dataframe = datavals.dataframe.append(row)
+                datavals.dataframe = pd.concat(datavals.dataframe, row)
 
     return _plotlyGraphCore(data, title, legend)
 
@@ -199,7 +200,7 @@ def plotlyAnimation(data: GraphData, title="", legend: bool=True, sizekey: Optio
                     row[data.colorkey] = color.color
                     row[data.sizekey] = 0
                     row[data.anim] = animval
-                    datavals.dataframe = datavals.dataframe.append(row)
+                    datavals.dataframe = pd.concat(datavals.dataframe, row)
 
     return _plotlyGraphCore(data, title, legend)
 
